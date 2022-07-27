030d9cf77c2642d0c6bae26b49616ed16faaa195
keosariel <keosariel@icloud.com>
2022-07-27 07:04:27 +0100

    fix: deploy multisig

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,2 +1,21 @@
 from brownie import accounts, MultiSigWallet
 
+def test_msig():
+    person1, person2, person3, person4, *_ = accounts
+
+    multisig = MultiSigWallet.deploy([person1, person2, person3], 2, {"from": person1})
+    
+    tx = multisig.submit(person4, 0.001, '', {"from": person2})
+    tx.wait(1)
+
+    tx = multisig.approve(0, {"from": person1})
+    tx.wait(1)
+
+    tx = multisig.approve(0, {"from": person3})
+    tx.wait(1)
+
+    tx = multisig.execute(0, {"from": person2})
+    tx.wait(1)
+
+def main():
+    test_msig()
